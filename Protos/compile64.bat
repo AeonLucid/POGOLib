b176046da387edfd2a93ba7dead5d665213023b9
AeonLucid <aeonlucid@outlook.com>
2016-07-15 22:19:04 +0200

    Recursive compiling \o/

--- Protos/compile64.bat
+++ Protos/compile64.bat
@@ -6,7 +6,7 @@ setlocal enabledelayedexpansion
 for /R . %%f in (*) do (
   set B=%%f
   ECHO Compiling "!B:%CD%\=!"..
-  ..\..\packages\Google.Protobuf.Tools.3.0.0-beta3\tools\windows_x86\protoc --csharp_out=..\..\POGOLib\Pokemon\Proto "!B:%CD%\=!"
+  ..\..\packages\Google.Protobuf.Tools.3.0.0-beta3\tools\windows_x64\protoc --csharp_out=..\..\POGOLib\Pokemon\Proto "!B:%CD%\=!"
 )
 
 pause
