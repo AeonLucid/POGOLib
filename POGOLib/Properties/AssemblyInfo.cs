3d822ded8ce26021ab82899afc9e9b08bb73cf72
AeonLucid <aeonlucid@outlook.com>
2016-07-23 05:13:48 +0200

    Update AssemblyInfo.

--- POGOLib/Properties/AssemblyInfo.cs
+++ POGOLib/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("POGOLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A community driven PokémonGo API Library written in C#.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AeonLucid")]
 [assembly: AssemblyProduct("POGOLib")]
 [assembly: AssemblyCopyright("Copyright © AeonLucid 2016")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")] 
 [assembly: AssemblyFileVersion("1.0.0.0")]
