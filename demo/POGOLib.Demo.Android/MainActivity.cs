069f3a6a901cc878bc9840fb589be0c680360f7b
AeonLucid <aeonlucid@outlook.com>
2016-09-05 19:47:38 +0200

    Started working on android demo.

---
+++ demo/POGOLib.Demo.Android/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace POGOLib.Demo.Android
+{
+    [Activity(Label = "POGOLib Android Demo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button _loginButton;
+        private EditText _username;
+        private EditText _password;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            SetContentView(Resource.Layout.Main);
+
+            _username = FindViewById<EditText>(Resource.Id.EditTextUsername);
+            _password = FindViewById<EditText>(Resource.Id.EditTextPassword);
+            _loginButton = FindViewById<Button>(Resource.Id.ButtonLogin);
+            _loginButton.Click += LoginButtonOnClick;
+        }
+
+        private void LoginButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            var username = _username.Text;
+            var password = _password.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                Toast.MakeText(Application, "Fill in an username and password.", ToastLength.Short).Show();
+                return;
+            }
+
+            // TODO: POGOLib  Login.GetSession(loginProvider, 51.507351, -0.127758)
+
+            Console.WriteLine("Does this work..?");
+        }
+    }
+}
+
