094387b743b62f86cc07607fcbe5976f0be9ac6b
Taras Bulba <tbulba.db@gmail.com>
2016-09-05 11:22:29 +0300

    User-Agent Generation

--- src/POGOLib/Util/Devices/Device.cs
+++ src/POGOLib/Util/Devices/Device.cs
@@ -1,7 +1,12 @@
-﻿namespace POGOLib.Util.Devices
+﻿using System;
+
+namespace POGOLib.Util.Devices
 {
     public class Device
     {
+        private string _userAgent;
+        private const string DefaultUserAgent = "Dalvik/2.1.0 (Linux; U; Android 5.0; Nexus 5 Build/LPX13D)";
+        private const string UserAgentFormat = "Dalvik/{0} (Linux; U; Android {1}; {2} Build/{3})";
 
         internal string AndroidBoardName { get; set; }
         internal string AndroidBootloader { get; set; }
@@ -16,7 +21,73 @@
         internal string FirmwareType { get; set; }
         internal string HardwareManufacturer { get; set; }
         internal string HardwareModel { get; set; }
-        internal string UserAgent { get; set; } = "Dalvik/2.1.0 (Linux; U; Android 5.0; Nexus 5 Build/LPX13D)"; // Device "nexus5".
 
+        internal string UserAgent
+        {
+            get
+            {
+                if (_userAgent == null)
+                {
+                    if (TryParseUserAgent(out _userAgent))
+                    {
+                        return _userAgent;
+                    }
+                    _userAgent = DefaultUserAgent;
+                }
+                return _userAgent;
+            }
+            set { _userAgent = value; }
+        }
+
+        private bool TryParseUserAgent(out string userAgent)
+        {
+            userAgent = null;
+            if (string.IsNullOrWhiteSpace(FirmwareFingerprint))
+            {
+                return false;
+            }
+            var fingerPrintParts = FirmwareFingerprint.Split('/');
+            if (fingerPrintParts.Length < 4)
+            {
+                return false;
+            }
+            if (!fingerPrintParts[2].Contains(":"))
+            {
+                return false;
+            }
+            try
+            {
+                string jvmVersion;
+                var androidVersion = fingerPrintParts[2].Split(':')[1];
+                if (androidVersion.StartsWith("2.1"))
+                {
+                    jvmVersion = "1.1.0";
+                }
+                else if (androidVersion.StartsWith("2.2"))
+                {
+                    jvmVersion = "1.2.0";
+                }
+                else if (androidVersion.StartsWith("2.3"))
+                {
+                    jvmVersion = "1.4.0";
+                }
+                else if (androidVersion.StartsWith("4"))
+                {
+                    jvmVersion = "1.6.0";
+                }
+                else
+                {
+                    jvmVersion = "2.1.0";
+                }
+
+                var build = fingerPrintParts[3];
+                userAgent = string.Format(UserAgentFormat, jvmVersion, androidVersion, DeviceModel, build);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
