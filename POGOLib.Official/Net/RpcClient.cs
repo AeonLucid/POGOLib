e47dbe65565331053ea4e560c03e5e4568612fcb
ibuckshot5 <ibuckshot5@comcast.net>
2017-06-22 16:41:38 -0400

    Update Pokehash maybe possibly probably (#91)
    * HASHERIGMO
    
    This needs no explination
    
    * Update RpcClient.cs
    
    Added for new GetInbox mesage sent on GMO
    
    * Update RpcClient.cs
    
    I forgot ToByteString... rip

--- POGOLib.Official/Net/RpcClient.cs
+++ POGOLib.Official/Net/RpcClient.cs
@@ -447,6 +447,13 @@ namespace POGOLib.Official.Net
                     }.ToByteString()
                 });
             }
+            
+            if (requestEnvelope.Requests.Count > 0 && requestEnvelope.Requests[0].RequestType == RequestType.GetMapObjects) {
+                requestEnvelope.Requests.Add(new Request {
+                    RequestType = RequestType.GetInbox,
+                    RequestMessage = new GetInboxMessage {}.ToByteString() // Honestly I have no idea what goes here
+                });
+            }
 
             return requestEnvelope;
         }
